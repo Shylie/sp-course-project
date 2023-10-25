6491f882156be63947fc7ea8032422122b24feb0
dnishkina <dnishkina@proton.me>
2023-10-25 14:01:47 -0400

    Changed formatting

--- commonlib/include/commonlib.h
+++ commonlib/include/commonlib.h
@@ -12,21 +12,21 @@ void cl_realloc(void** ptr, size_t size);
 struct array
 {
 	size_t element_size;
-	uint8_t* data;
+	uint8_t *data;
 	size_t data_size;
 	size_t used_data_size;
 };
 
 struct array array_new(size_t element_size, size_t initial_count);
-void array_del(struct array* arr);
+void   array_del(struct array* arr);
 size_t array_size(const struct array* arr);
 size_t array_max_size(const struct array* arr);
-void array_resize(struct array* arr, size_t new_max_size);
-void* array_at(struct array* arr, size_t index);
-void array_append(struct array* arr, const void* element);
-void array_remove(struct array* arr, size_t index);
-void* array_find(struct array* arr, const void* element_to_find);
-void array_foreach(struct array* arr, void (*fn)(void* elem, size_t elem_size, void* ud), void* ud);
+void   array_resize(struct array* arr, size_t new_max_size);
+void   *array_at(struct array* arr, size_t index);
+void   *array_find(struct array* arr, const void* element_to_find);
+void   array_append(struct array* arr, const void* element);
+void   array_remove(struct array* arr, size_t index);
+void   array_foreach(struct array* arr, void (*fn)(void* elem, size_t elem_size, void* ud), void* ud);
 
 struct map
 {
@@ -36,7 +36,7 @@ struct map
 
 struct map map_new(size_t element_size);
 void map_del(struct map* m);
-void* map_get(struct map* m, const char* key, size_t keylen);
+void *map_get(struct map* m, const char* key, size_t keylen);
 void map_set(struct map* m, const char* key, size_t keylen, const void* elem);
 
 #endif//COMMONLIB_H
