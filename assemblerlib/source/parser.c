4943a4097700684cce344097ed0b4edbaf562498
dnishkina <dnishkina@proton.me>
2023-11-06 12:59:52 -0500

    stuff

---
+++ assemblerlib/source/parser.c
@@ -0,0 +1,26 @@
+#include "assemblerlib-private.h"
+
+void parse_file(struct assembler_state* state, const char* source)
+{
+
+}
+
+void parse_line(struct assembler_state* state, const char* line)
+{
+
+}
+
+void parse_label(struct assembler_state* state, const char* label)
+{
+
+}
+
+void parse_operation(struct assembler_state* state, const char* operation)
+{
+
+}
+
+void parse_operand(struct assembler_state* state, const char* operand)
+{
+
+}
